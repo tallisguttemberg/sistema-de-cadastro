5550481bb5eb14bdf2a5c6c8bb433469b8352b8f
Tallis Guttemberg <94991584+tallisguttemberg@users.noreply.github.com>
2022-06-20 12:18:22 -0300

    Atualizando o app.ls

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de geranciamento de jogos")
+console.log("Aplicação está se conectando ao banco de dados")
+//Mais um console de log de teste
+console.log("O Paulo fez o commit sem você ver na aula!")
