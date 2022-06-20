423b5b9b987fcc1553a51f81a0166823d87863f0
TallisGuimaraes <tallis.guimaraes@perinity.com>
2022-06-20 13:24:33 -0300

    ultimo teste de anvio

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Rodando o sistema de geranciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
-//Mais um console de log de teste
-console.log("O Paulo da ALURA fez o commit sem você ver na aula!")
+//Mais um console de log de teste pra testar
+console.log("O Paulo da ALURA fez o commit sem você ver na aula. Olha lá depois!")
