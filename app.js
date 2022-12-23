9f649813434bfedb82201e947e15cdea8dc87941
Tallis Guttemberg <tallis.guimaraes@gmail.com>
2022-12-23 19:36:00 -0300

    Atualizando o app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando sistema de gerenciamento de jogos")
+console.log("aplicação está se conectando ao banco de dados")
+// mais um console log de teste
+console.log("O paulo  fez esse cmmit sem vcverna aula!")
